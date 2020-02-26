ff89781a475aad42fe7023dc41ba75dec9442e2c
YUG25 <61495515+YUG25@users.noreply.github.com>
2020-02-26 14:45:04 +0530

    hi

---
+++ 1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Iterator Program
+
+# Defining the list to be iterated
+
+list1 = ['Mumbai', 'Pune', 'Delhi', 'Patna']
+iteratorObject = iter(list1)
+
+# Iteration
+
+print next(iteratorObject)
+print next(iteratorObject)
+print next(iteratorObject)
+print next(iteratorObject)
